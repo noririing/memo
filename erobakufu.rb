67868367b0ff0ab0cc1570faa4a38da2df00e226
noririing <noririing@gmail.com>
2014-10-09 20:16:56 +0900

    change source

--- erobakufu.rb
+++ erobakufu.rb
@@ -2,42 +2,59 @@
 
 require "open-uri"
 require "nokogiri"
+require "fileutils"
 
-doc = Nokogiri.HTML(open("http://bakufu.jp/"))
+INDEX_URL = 'http://bakufu.jp/'
+MATCH_URL = 'img\.bakufu\.jp'
+SAVE_DIR = './img'
 
-def get_jpg(url)
+def get_jpg_url(url)
   d = Nokogiri.HTML(open(url))
+  a = Array.new
   d.xpath('//a').each do | n |
     s = n.attribute('href').value
-    if  /img\.bakufu\.jp/ =~ s
-      #puts s
-      save_jpg s
+    if  /MATCH_URL/ =~ s
+      a << s
     end
   end
+  return a
 end
 
-def save_jpg(url)
-    filename = File.basename(url)
-    open(filename, 'wb') do |file|
-        open(url) do |data|
-            file.write(data.read)
+
+def save_jpg(dirpath,url)
+  fname = File.basename(url)
+  open(dirpath + '/' + fname, 'wb') do |f|
+      open(url) do |d|
+          f.write(d.read)
+      end
+  end
+  sleep 1
+end
+
+
+def get_targeturl
+  d = Nokogiri.HTML(open(INDEX_URL))
+  h = Hash::new
+  d.xpath('//header').each do |n|
+    unless n.css('a').text.strip == "" then
+      h[n.css('a').text.strip] =  n.css('a').attribute('href').value
+    end
+  end
+  return h
+end
+
+
+def dl_img()
+  h = get_targeturl
+  h.each_pair { | key , url |
+    fdpath = SAVE_DIR + '/' + key[0,50]
+    unless FileTest.exist?(fdpath) then
+      FileUtils.mkdir(fdpath)
+      a = get_jpg_url url
+      a.each do |url|
+        save_jpg fdpath url
       end
     end
+  }
 end
-
-h = Hash::new
-
-doc.xpath('//header').each do | node |
-  #p node.css('a').text.strip
-  #p node.css('a').attribute('href').value
-  h[node.css('a').text.strip] =  node.css('a').attribute('href').value
-end
-
-#p h
-
-h.each_pair { | key , value |
-  unless key == "" then
-    get_jpg value
-  end
-}
 
